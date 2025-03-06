1abff9f46904fc6a24f6e64270c1c5352cde9943
JOKER2l <dudu.rosalima2020@gmail.com>
2025-03-06 18:56:39 -0300

    Initial commit

---
+++ rola_debadged_pack_files_9/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Rola Mods Debadged Pack Files 1'
+author 'Rola Mods'
+ 
+files {
+    'data/**/*.meta'
+}
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+
+dependency '/assetpacks'
